d91b7ac81d2a8307cdfab743ae9312f1df6cbb1f
Bernie <itdalde@gmail.com>
2022-10-06 08:05:42 +0800

    set fetch occasions to public

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,10 @@ Route::group(['prefix' => 'v1','middleware' => ['cors']], function () {
     Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
     Route::post('/resend-confirmation','Auth\ApiAuthController@resendConfirmation')->name('resend.confirmation.api');
     Route::post('/google-login', 'Auth\ApiAuthController@googleLogin')->name('google.login.api');
+    Route::get('/occasions', [OccasionController::class,'index'])->name('occasions.list.api');
 });
 Route::group(['prefix' => 'v1', 'middleware' => ['auth:api','cors']], function() {
     Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
     Route::get('/me', 'Auth\ApiAuthController@me')->name('me.api');
     Route::get('/users', [UserController::class,'userList'])->name('users.list.api');
-    Route::get('/occasions', [OccasionController::class,'index'])->name('occasions.list.api');
 });
