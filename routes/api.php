f92452cc7a7df7d9da1834c88cd857a12207f6bd
ts-roms <rcena.msi@gmail.com>
2022-11-09 14:13:18 +0800

    - added default endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\OccasionController;
+use App\Http\Controllers\OccasionEventController;
+use App\Http\Controllers\OccasionEventReviewsController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\MembershipController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,3 +36,83 @@ Route::group(['prefix' => 'v1', 'middleware' => ['auth:api','cors']], function()
     Route::get('/me', 'Auth\ApiAuthController@me')->name('me.api');
     Route::get('/users', [UserController::class,'userList'])->name('users.list.api');
 });
+
+//  INFO: Membership Endpoints
+Route::group(['prefix' => 'v1/membership', 'middleware' => ['cors']], function() {
+    Route::get('/', [MembershipController::class, 'index'])->name('index');
+    Route::post('/create', [MembershipController::class, 'create'])->name('create');
+    Route::post('/store', [MembershipController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [MembershipController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [MembershipController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [MembershipController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [MembershipController::class, 'destroy'])->name('destroy');
+    Route::get('/members', [MembershipController::class, 'getMembers'])->name('membership');
+});
+
+//  INFO: Occasion Endpoints
+Route::group(['prefix' => 'v1/occasion', 'middleware' => ['cors']], function() {
+    Route::get('/', [OccasionController::class, 'index'])->name('index');
+    Route::post('/create', [OccasionController::class, 'create'])->name('create');
+    Route::post('/store', [OccasionController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [OccasionController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [OccasionController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [OccasionController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [OccasionController::class, 'destroy'])->name('destroy');
+    Route::get('/demo', [OccasionController::class, 'demo'])->name('demo');
+});
+
+//  INFO: OccasionEvent Endpoints
+Route::group(['prefix' => 'v1/occasion-event', 'middleware' => ['cors']], function() {
+    Route::get('/', [OccasionEventController::class, 'index'])->name('index');
+    Route::post('/create', [OccasionEventController::class, 'create'])->name('create');
+    Route::post('/store', [OccasionEventController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [OccasionEventController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [OccasionEventController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [OccasionEventController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [OccasionEventController::class, 'destroy'])->name('destroy');
+});
+
+//  INFO: OccasionEventReviews Endpoints
+Route::group(['prefix' => 'v1/occasion-event-reviews', 'middleware' => ['cors']], function() {
+    Route::get('/', [OccasionEventReviewsController::class, 'index'])->name('index');
+    Route::post('/create', [OccasionEventReviewsController::class, 'create'])->name('create');
+    Route::post('/store', [OccasionEventReviewsController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [OccasionEventReviewsController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [OccasionEventReviewsController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [OccasionEventReviewsController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [OccasionEventReviewsController::class, 'destroy'])->name('destroy');
+});
+
+//  INFO: Order Endpoints
+Route::group(['prefix' => 'v1/order', 'middleware' => ['cors']], function() {
+    Route::get('/', [OrderController::class, 'index'])->name('index');
+    Route::post('/create', [OrderController::class, 'create'])->name('create');
+    Route::post('/store', [OrderController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [OrderController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [OrderController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [OrderController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [OrderController::class, 'destroy'])->name('destroy');
+});
+
+//  INFO: Service Endpoints
+Route::group(['prefix' => 'v1/service', 'middleware' => ['cors']], function() {
+    Route::get('/', [ServiceController::class, 'index'])->name('index');
+    Route::post('/create', [ServiceController::class, 'create'])->name('create');
+    Route::post('/store', [ServiceController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [ServiceController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [ServiceController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [ServiceController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [ServiceController::class, 'destroy'])->name('destroy');
+});
+
+//  INFO: Transaction Endpoints
+Route::group(['prefix' => 'v1/transaction', 'middleware' => ['cors']], function() {
+    Route::get('/', [TransactionController::class, 'index'])->name('index');
+    Route::post('/create', [TransactionController::class, 'create'])->name('create');
+    Route::post('/store', [TransactionController::class, 'store'])->name('store');
+    Route::get('/show/{id}', [TransactionController::class, 'show'])->name('show');
+    Route::get('/edit/{id}', [TransactionController::class, 'edit'])->name('edit');
+    Route::put('/update/{id}', [TransactionController::class, 'update'])->name('update');
+    Route::post('/destroy/{id}', [TransactionController::class, 'destroy'])->name('destroy');
+});
+
