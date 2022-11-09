7e68e0113188497ac55d2d7d9cea65126a43bb8e
ts-roms <rcena.msi@gmail.com>
2022-11-09 22:08:29 +0800

    - added new table

--- app/Models/OccasionEvent.php
+++ app/Models/OccasionEvent.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class OccasionEvent extends Model
 {
@@ -11,5 +12,10 @@ class OccasionEvent extends Model
 
     public function occasion() {
         return $this->belongsTo(Occasion::class);
+    }
+
+    public function occasionEventPrice(): HasMany
+    {
+        return $this->hasMany(OccasionEventPrice::class);
     }
 }
