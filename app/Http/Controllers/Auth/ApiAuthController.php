92198d62eab594801d8a0234c6f7795dd33e031d
Bernie <bdalde@tastradesoft.com>
2022-12-09 14:27:23 +0800

    Added confirm email

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -36,28 +36,163 @@ class ApiAuthController extends Controller
         }
         $request['password']=Hash::make($request['password']);
         $request['remember_token'] = Str::random(10);
+        $confirmationCode = Uuid::uuid4();
         $user = User::create([
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
             'phone_number' => $data['phone_number'],
             'password' => bcrypt($data['password']),
-            'confirmation_code' => Uuid::uuid4(),
+            'confirmation_code' => $confirmationCode,
             'confirmed' => false
         ]);
         if (config('auth.users.default_role')) {
             $user->roles()->attach(Role::firstOrCreate(['name' =>'user']));
         }
 
-        $to = $data['email'];
-        $subject = "Welcome";
-        $txt = "Welcome to Reserve";
-        $headers = "From: contactus@reservegcc.com";
-
-        mail($to,$subject,$txt,$headers);
-//        $user->notify(new ConfirmEmail());
+        $this->sendEmail($data,$confirmationCode);
         $response = ['message' => 'Successfully created. Please check your email to confirm'];
         return response()->json($response,200);
+    }
+
+    private function sendEmail($data,$confirmationCode) {
+
+        $to = $data['email'];
+        $subject = "Welcome to Reserve";
+        $style = '<style type="text/css">
+          body,
+          table,
+          td,
+          a {
+            -ms-text-size-adjust: 100%; /* 1 */
+            -webkit-text-size-adjust: 100%; /* 2 */
+          }
+          table,
+          td {
+            mso-table-rspace: 0pt;
+            mso-table-lspace: 0pt;
+          }
+          img {
+            -ms-interpolation-mode: bicubic;
+          }
+          a[x-apple-data-detectors] {
+            font-family: inherit !important;
+            font-size: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+            color: inherit !important;
+            text-decoration: none !important;
+          }
+          div[style*="margin: 16px 0;"] {
+            margin: 0 !important;
+          }
+          body {
+            width: 100% !important;
+            height: 100% !important;
+            padding: 0 !important;
+            margin: 0 !important;
+          }
+          table {
+            border-collapse: collapse !important;
+          }
+          a {
+            color: #1a82e2;
+          }
+          img {
+            height: auto;
+            line-height: 100%;
+            text-decoration: none;
+            border: 0;
+            outline: none;
+          }
+          </style>';
+        $confirm = route('confirm', [$confirmationCode]);
+        $message = ' <!DOCTYPE html>
+        <html>
+        <head>
+
+          <meta charset="utf-8">
+          <meta http-equiv="x-ua-compatible" content="ie=edge">
+          <title>Email Confirmation</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          '.$style.'
+
+        </head>
+            <body style="background-color: #e9ecef;">
+              <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
+                   Hello '.$data["first_name"]." ".$data["last_name"].',
+              </div>
+              <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td align="center" bgcolor="#e9ecef">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                      <tr>
+                        <td align="center" valign="top" style="padding: 36px 24px;">
+                          <a href="https://reservegcc.com" target="_blank" style="display: inline-block;">
+                            <img src="https://reservegcc.com/assets/landing/img/logo-black.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
+                          </a>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td align="center" bgcolor="#e9ecef">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                      <tr>
+                        <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0;  border-top: 3px solid #d4dadf;">
+                          <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Confirm Your Email Address</h1>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+                <tr>
+                  <td align="center" bgcolor="#e9ecef">
+                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                      <tr>
+                        <td align="left" bgcolor="#ffffff" style="padding: 24px;  font-size: 16px; line-height: 24px;">
+                          <p style="margin: 0;">Tap the button below to confirm your email address. If you didn\'t create an account with <a href="https://reservegcc.com">Paste</a>, you can safely delete this email.</p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="left" bgcolor="#ffffff">
+                          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                              <td align="center" bgcolor="#ffffff" style="padding: 12px;">
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                  <tr>
+                                    <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
+                                      <a href="'.$confirm.'" target="_blank" style="display: inline-block; padding: 16px 36px;  font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm</a>
+                                    </td>
+                                  </tr>
+                                </table>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td align="left" bgcolor="#ffffff" style="padding: 24px;  font-size: 16px; line-height: 24px;">
+                          <p style="margin: 0;">If that doesnt work, copy and paste the following link in your browser:</p>
+                          <p style="margin: 0;"><a href="'.$confirm.'" target="_blank">'.$confirm.'</a></p>
+                        </td>
+                      </tr>
+                    </table>
+                    </td>
+                    </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </body>
+            </html>
+        ';
+
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= 'From: <contactus@reservegcc.com>' . "\r\n";
+        mail($to,$subject,$message,$headers);
     }
 
     public function resendConfirmation(Request $request) {
