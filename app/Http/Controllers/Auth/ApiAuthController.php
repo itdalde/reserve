75c30a326ca876ea459ce5cb7c8b17070a50d798
Bernie <bdalde@tastradesoft.com>
2022-12-10 12:25:20 +0800

    Added reset forgot password

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon as Carbon;
 use http\Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
