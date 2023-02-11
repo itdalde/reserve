90217d6052ee7b01c459d0714c92d8cadeac9c15
iamroms <rcena@startechup.com>
2023-02-11 21:37:47 +0800

    - update on orders

--- app/Console/Commands/OrderScheduler.php
+++ app/Console/Commands/OrderScheduler.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Auth\User\User;
 use App\Models\Order;
+use App\Utility\NotificationUtility;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class OrderScheduler extends Command
@@ -39,9 +42,16 @@ class OrderScheduler extends Command
     public function handle()
     {
         $this->info('Check orders daily');
-        $order = Order::first();
-
-        // 
-        return $order;
+        $orders = Order::where('status', 'pending')->where('updated_at', '>=', Carbon::now()->addDay()->toDateTimeString())->get();
+        foreach($orders as $order) {
+            $response = [
+                "status" => "success",
+                "message" => "Notification invoke for pending orders",
+                "data" => ['order' => $order ]
+            ];
+            $fcmTokens = User::where('user_id', $order->user_id)->whereNotNull('fcm_token')->pluck('fcm_token')->toArray();
+            NotificationUtility::sendNotification('Pending Order', 'You still have pending order in your cart.', $fcmTokens, $response);
+        }   
+        $this->info('Notification invoke for users');
     }
 }
