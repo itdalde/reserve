586780c44a77f9c41ae929fa7660b0db74acd153
iamroms <rcena@startechup.com>
2023-02-20 21:43:31 +0800

    - update on mobile view and status

--- database/migrations/2022_12_15_005404_create_order_items.php
+++ database/migrations/2022_12_15_005404_create_order_items.php
@@ -21,8 +21,8 @@ class CreateOrderItems extends Migration
             $table->dateTime('schedule_start_datetime');
             $table->dateTime('schedule_end_datetime');
             $table->integer('guests')->unsigned();
-            $table->enum('timeline', ['order-placed', 'processing', 'order-completed'])->default('order-placed');
-            $table->enum('status', ['accepted', 'pending', 'processing', 'cancelled', 'completed'])->default('accepted');
+            $table->enum('timeline', ['order-placed', 'order-accepted', 'order-declined', 'processing', 'order-completed'])->default('order-placed');
+            $table->enum('status', ['placed', 'accepted', 'declined', 'pending', 'processing', 'cancelled', 'completed'])->default('accepted');
             $table->tinyInteger('is_custom')->default(0);
             $table->timestamps();
         });
