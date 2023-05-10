d8fd199176d06d990322cc1f0b69fbe080692508
Bernie <bdalde@tastradesoft.com>
2023-05-10 16:34:57 +0800

    Added creator map

--- app/Models/Notes.php
+++ app/Models/Notes.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Auth\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Notes extends Model
 {
     use HasFactory;
+
+    public function creator() {
+        return $this->hasOne(User::class,'created_by','id');
+    }
 }
