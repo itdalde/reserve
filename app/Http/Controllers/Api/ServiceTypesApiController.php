486612c8657f7b066b90d91a2e23253fea8eb86d
Bernie <bdalde@tastradesoft.com>
2023-07-19 21:33:01 +0800

    Update

--- app/Http/Controllers/Api/ServiceTypesApiController.php
+++ app/Http/Controllers/Api/ServiceTypesApiController.php
@@ -75,8 +75,8 @@ class ServiceTypesApiController extends Controller
                         if ($provider['id'] == $service['company_id']) {
                             $providers[$key]['base_price'] = (int) $provider['base_price'];
                             $availableDateObj = AvailableDates::where('service_id', $service['id'])
-                                ->where('ad.status', 1)
-                                ->where('ad.date_obj', '<>', null);
+                                ->where('status', 1)
+                                ->where('date_obj', '<>', null);
 
                             if (isset($data['from']) && isset($data['to'])) {
                                 $availableDateObj->whereBetween('date_obj', [$data['from'], $data['to']]);
