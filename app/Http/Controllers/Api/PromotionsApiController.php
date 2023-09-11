b1de370100661ebde1a5244febca05ebac9f45c2
Bernie <bdalde@tastradesoft.com>
2023-09-11 17:58:32 +0800

    update

--- app/Http/Controllers/Api/PromotionsApiController.php
+++ app/Http/Controllers/Api/PromotionsApiController.php
@@ -47,6 +47,11 @@ class PromotionsApiController extends Controller
     public function getPromotionByUserAndCode(Request $request, $user_id, $promotion_code): JsonResponse
     {
         try {
+
+            $hasPromo = Promotions::where('code', $promotion_code)->first();
+            if(!$hasPromo) {
+                return sendError('Promo not found', 'Promo code invalid', 422);
+            }
             $response = Promotions::leftJoin('user_promotions as up', 'promotions.id', '=', 'up.promotion_id')
                 ->where('promotions.code', '=', $promotion_code)
                 ->where('up.user_id', '=', $user_id)
